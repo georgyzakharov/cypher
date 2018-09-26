f79e2956a2ff71617e7146e639808ffbb00e52f3
ripcord <jfhead97@gmail.com>
2018-09-26 19:11:03 -0400

    Added comments.

--- backend/src/DockerRun.java
+++ backend/src/DockerRun.java
@@ -1,21 +1,74 @@
+/*
+Author: Jacob Hill ("ripcord")
+Date of Revision: 09/26/2018
+Project: Cypher
+Group Members:
+	Austin Monson ("Sannity")
+	Dylan Navy ("navyd")
+	Georgy Zakharov ("georgyzakharov")
+Description:
+	This program is designed to take a source file,
+	which is defined here as a file with the extension
+	".java", ".c", ".cpp", or ".py", and attempt to
+	compile/interpret and execute that file in a
+	virtual Docker container. The program requires the
+	absolute file path of the source file. The directory
+	immediately above the source code file is treated
+	as the "team" directory, that is the directory
+	in which the created Dockerfile is stored in
+	and which the program copies into the created
+	Docker container. For example, consider a file
+	with the following path:
+		"/home/cypher/src/foobar.java"
+	The contents of the directory "src/" would be
+	copied into a Docker container at runtime and
+	the Dockerfile for "foobar.java" (named by default
+	in accordance with Docker convention as simply
+	"Dockerfile") would be created and stored within
+	"src/".
+Bugs:
+	None known.
+Future Development:
+	1. Add support for returned line separators (i.e.,
+	newlines or "\n") in subprocess input, which is
+	piped to STDOUT on a CLI.
+*/
+
+
 import java.io.*;
 
+//'final' so as not to extend this class
 public final class DockerRun {
 
+	//'imageTag' is a unique alphanumeric
+	//identified applied to Docker images
+	//for simplified reference vs. default
+	//image IDs
+	//
+	//'errors' is a String that acts as an
+	//error log
+	//
+	//'pb' establishes the context for the
+	//subprocesses that invoke Docker
 	private static String imageTag;
 	private static String errors;
 	private static ProcessBuilder pb;
 
+	//Does nothing
 	private DockerRun() {
 	}
 
+	//Returns a file extension
 	public static String getExtension (String s) {
 		s = s.substring(s.lastIndexOf(File.separator), s.length());
 		return s.substring(s.lastIndexOf("."), s.length());
 	}
 
+	//Creates the Dockerfile for a Docker image
 	public static boolean writeDockerfile(String source) {
 		try {
+			//'teamDir' is the directory immediately above the
+			//source code file
 			File sourceCode = new File(source);
 			File teamDir = new File(source.substring(0, source.lastIndexOf(File.separator)));
 
@@ -34,6 +87,7 @@ public final class DockerRun {
 
 				FileOutputStream out = new FileOutputStream(Dockerfile);
 
+				//Subsequent 3 strings are used when writing to the Dockerfile
 				String copyToContainerDir = new String(teamDir.toString().substring(teamDir.toString().lastIndexOf(File.separator), teamDir.toString().length()));
 				String sourceFileName = new String(source.substring(source.lastIndexOf(File.separator), source.length()));
 				String sourceFileNameNoExtension = sourceFileName.substring(1, sourceFileName.lastIndexOf("."));
@@ -99,6 +153,8 @@ public final class DockerRun {
 		}
 	}
 
+	//Method used to invoke Docker image building
+	//and Docker container instantiation
 	public static String runDocker(String source) {
 		if (buildDockerImage(source)) {
 			return runDockerContainer();
@@ -108,19 +164,20 @@ public final class DockerRun {
 		}
 	}
 
+	//Mthod for building a Docker image from a provided
+	//source code file path
 	public static boolean buildDockerImage(String source) {
 		try {
 			imageTag = new String(source.substring(source.lastIndexOf(File.separator) + 1, source.length()).toLowerCase());
 			imageTag = imageTag.substring(0, imageTag.lastIndexOf(".")) + "-" + imageTag.substring(imageTag.lastIndexOf(".") + 1, imageTag.length());
 			pb = new ProcessBuilder();
-			//pb.redirectErrorStream(true);
 
 			pb.command("docker", "build", "-t", imageTag, ".");
 			pb.directory(new File(source.substring(0, source.lastIndexOf(File.separator))));
 
 			Process p = pb.start();
 
-			//BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			//Getting the errors (if any) from the process
 			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 
 			String s = null;
@@ -129,17 +186,15 @@ public final class DockerRun {
 				errors = errors + s;
 			}
 
-			//PROBLEM: 'if (errors != null || !errors.isEmpty())'
+			//LOGIC PROBLEM: 'if (errors != null || !errors.isEmpty())'
 			if (errors != null && !errors.isEmpty()) {
 				p.waitFor();
 				p.destroy();
-				//return errors;
 				return false;
 			}
 			else {
 				p.waitFor();
 				p.destroy();
-				//return runDockerContainer(pb, imageTag);
 				return true;
 			}
 
@@ -148,14 +203,12 @@ public final class DockerRun {
 		catch (IOException e) {
 			errors = new String("<IOException> thrown in 'buildDockerImage()'!");
 			e.printStackTrace();
-			//return "Error!";
 			return false;
 		}
 
 		catch (Exception e) {
 			errors = new String("<Exception> thrown in 'buildDockerImage()'!");
 			e.printStackTrace();
-			//return "Error!";
 			return false;
 		}
 	}
@@ -164,11 +217,11 @@ public final class DockerRun {
 	//image has been built in 'buildDockerImage()'
 	private static String runDockerContainer() {
 		try {
-			//pb.command("/bin/sh", "-c", "docker", "run", "--rm", imageTage);
 			pb.command("docker", "run", "--rm", imageTag);
 
 			Process p = pb.start();
 
+			//Getting errors (if any) and output from the process
 			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 
@@ -194,6 +247,8 @@ public final class DockerRun {
 				p.destroy();
 				return results;
 			}
+			//The default 'else' case below is unlikely but is
+			//provided as insurance for additional error handling
 			else {
 				p.waitFor();
 				p.destroy();
@@ -214,8 +269,13 @@ public final class DockerRun {
 		}
 	}
 
+	//The 'compilation & execution' method,
+	//which invokes the 'runDocker()' method
 	public static String compExec(String s) {
 
+		//String 'source' is created so as
+		//to pass a brand-new guranteed String
+		//object
 		String source = new String(s);
 
 		if (writeDockerfile(source)) {
@@ -226,6 +286,7 @@ public final class DockerRun {
 		}
 	}
 
+	//Solely a testing method, will be removed in future versions
 	public static void main(String[] args) {
 
 		System.out.println(DockerRun.compExec(args[args.length-1]));
@@ -233,4 +294,3 @@ public final class DockerRun {
 		return;
 	}
 }
-
