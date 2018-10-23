18127f53531bd0799c1b3bad47a2e4228195f2f7
ddn12 <ddn12@192.168.199.127>
2018-10-23 17:45:31 -0400

    Revised the submission handler so it's not garbage

--- cypher-backend/src/main/java/edu/sunypoly/cypher/backend/service/ProgCompSubmissionHandler.java
+++ cypher-backend/src/main/java/edu/sunypoly/cypher/backend/service/ProgCompSubmissionHandler.java
@@ -1,5 +1,8 @@
 package edu.sunypoly.cypher.backend.service;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -7,16 +10,18 @@ public class ProgCompSubmissionHandler implements Runnable
 {
 	public ProgCompSubmissionHandler() 
 	{
+		Completed = new LinkedList<ProgCompSubmission>();
+		subs = new LinkedBlockingQueue<Runnable>();
 		int Cores = Runtime.getRuntime().availableProcessors();
-		executor = new ThreadPoolExecutor(Cores, Cores*2, 10, TimeUnit.SECONDS, this.subs);
 		
-		
+		executor = new ThreadPoolExecutor(Cores, Cores*2, 10, TimeUnit.SECONDS, this.subs);	
 		
 	}
+	
 	public ProgCompSubmission addSubmission(ProgCompSubmission sub)
 	{
 		this.subs.offer(sub);
-		
+
 		return sub;
 	}
 	
@@ -28,7 +33,9 @@ public class ProgCompSubmissionHandler implements Runnable
 			{
 				try
 				{
-					executor.execute(subs.take());
+					ProgCompSubmission Temp = (ProgCompSubmission) subs.take();
+					executor.execute(Temp);
+					Completed.add(Temp);
 				}
 				catch (InterruptedException e)
 				{
@@ -39,9 +46,8 @@ public class ProgCompSubmissionHandler implements Runnable
 	}
 	
 	
-	
-	
 	private ThreadPoolExecutor executor;
 	private static BlockingQueue<Runnable> subs;
+	private static Queue<ProgCompSubmission> Completed;
 	
 }
