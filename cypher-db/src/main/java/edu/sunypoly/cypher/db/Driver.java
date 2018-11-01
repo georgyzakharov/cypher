f3af116ca69ac3124a3d54e2313d758eb73427f0
georgezakharov <georgezakharov@192.168.218.81>
2018-11-01 14:35:51 -0400

    throws AlreadyExistsException,

--- cypher-db/src/main/java/edu/sunypoly/cypher/db/Driver.java
+++ cypher-db/src/main/java/edu/sunypoly/cypher/db/Driver.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.nio.file.Files;
 
 public class Driver {
-	public static void main(String[] args) throws AlreadyExistsException, NullInputException, InvalidDataException {
+	public static void main(String[] args) throws AlreadyExistsException, NullInputException, InvalidDataException, DoesNotExistException {
 	Mis Manager = new Mis("jdbc:mysql://localhost/cypher_db?useSSL=false", "cypher", "cypher");
 	
 	Manager.Team.create("team1","password");
