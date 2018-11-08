6f9e9213abd9a44d604dca88c3d102d2a9491b34
Sannity <austinmonson1884@gmail.com>
2018-11-08 15:29:25 -0500

    Fixed an error in account creation

--- cypher-db/src/main/java/edu/sunypoly/cypher/db/Driver.java
+++ cypher-db/src/main/java/edu/sunypoly/cypher/db/Driver.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 
 public class Driver {
 	public static void main(String[] args) throws AlreadyExistsException, NullInputException, InvalidDataException, DoesNotExistException {
-	Mis Manager = new Mis("jdbc:mysql://localhost/cypher_db?useSSL=false", "root", "DamienBro");
+	Mis Manager = new Mis("jdbc:mysql://localhost/cypher_db?useSSL=false", "cypher", "cypher");
 	
 	Manager.Team.create("team1","Password123");
 	System.out.println(Manager.Team.getId("team1"));
