2d129fbc45206daa9db28aab12d902716feddf15
Sannity <austinmonson1884@gmail.com>
2018-12-06 14:41:32 -0500

    add login verification

--- cypher-frontend/src/main/java/edu/sunypoly/cypher/frontend/controller/HomeController.java
+++ cypher-frontend/src/main/java/edu/sunypoly/cypher/frontend/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import edu.sunypoly.cypher.frontend.service.ApplicationExecutorService;
 
+import edu.sunypoly.cypher.db.*;
 @Controller
 public class HomeController {
 
@@ -16,25 +17,59 @@ public class HomeController {
 	@Resource
 	private ApplicationExecutorService service;
 
+	Mis db_manager = new Mis("jdbc:mysql://localhost/cypher_db?useSSL=false", "cypher", "cypher");
+
 	// Home page with all the UI
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
-	public ModelAndView getLogin() {
-
+	public ModelAndView getLogin() 
+	{
 		ModelAndView model = new ModelAndView("log_in");
-
 		return model;
 	}
 
 	@RequestMapping(value = { "/mainpage" }, method = RequestMethod.GET)
-	public ModelAndView getHome(int typeOfUser) {
-		if (typeOfUser == 0) {
-			ModelAndView model = new ModelAndView("team_landing");
+	public ModelAndView getHome(int typeOfUser, String username, String password) 
+	{
+		boolean userExists = false;
+		
+		try
+		{
+			userExists = db_manager.Account.validate(username, password);
+		}
+		catch(NullInputException e)
+		{
+			ModelAndView model = new ModelAndView("error");
 			return model;
-		} else if (typeOfUser == 1) {
-			ModelAndView model = new ModelAndView("proctor_landing");
+		}
+		catch(InvalidDataException e)
+		{
+			ModelAndView model = new ModelAndView("error");
+			return model;
+		}
+		catch(DoesNotExistException e)
+		{
+			ModelAndView model = new ModelAndView("error");
+			return model;
+		}
+
+		if(userExists)
+		{
+			if (typeOfUser == 0) 
+			{
+				ModelAndView model = new ModelAndView("team_landing");
+				return model;
+			} 
+			else if (typeOfUser == 1) 
+			{
+				ModelAndView model = new ModelAndView("proctor_landing");
+				return model;
+			}
+		}
+		else
+		{
+			ModelAndView model = new ModelAndView("error");
 			return model;
 		}
 		return null;
 	}
-
 }
